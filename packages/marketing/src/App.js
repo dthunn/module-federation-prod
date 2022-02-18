f008841abff2c65d940d517b60e220a89dda46d6
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2022-02-17 17:42:32 -0700

    fixed production class names

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { StylesProvider } from '@material-ui/core'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+})
 
 import Landing from './Landing'
 import Pricing from './Pricing'
@@ -8,7 +12,7 @@ import Pricing from './Pricing'
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             {/* change */}
